6ec403fbfd713381d6468c95b54ad76b4f7c883b
Naikajork <inert66@hotmail.com>
2020-12-18 23:23:01 +0700

    Create Exercise4

---
+++ Assignment/Exercise4_Kriangkrai_T.py
@@ -0,0 +1,8 @@
+FEScore = 78.7
+GBScore = 83.5
+ICSScore = 81.2
+CPScore = 85.6
+print("Foundation English :", FEScore)
+print("General Business :", GBScore)
+print("Introduction to Computer System :", ICSScore)
+print("Computer Programing :", CPScore)
