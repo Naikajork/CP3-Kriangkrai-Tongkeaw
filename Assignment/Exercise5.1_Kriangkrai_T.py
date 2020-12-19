169997ed253862bd13855ba559736b8174791722
Naikajork <inert66@hotmail.com>
2020-12-19 21:19:27 +0700

    Create Exercise5.1_Kriangkrai_T.py

---
+++ Assignment/Exercise5.1_Kriangkrai_T.py
@@ -0,0 +1,10 @@
+x = int(input("X = "))
+y = int(input("Y = "))
+Add = x + y
+Min = x - y
+Mul = x * y
+Div = x / y
+print("X + Y = ", Add)
+print("X - Y = ", Min)
+print("X * Y = ", Mul)
+print("X / Y = ", Div)
