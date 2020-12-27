0036c44fe5df5d6c3f8b17d2bb9739590b4a2e74
Naikajork <inert66@hotmail.com>
2020-12-27 20:06:11 +0700

    Create Lecture50_Kriangkrai_T.py

---
+++ Assignment/Lecture50_Kriangkrai_T.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+addNumber(5,3)
+minusNumber(7,4)
+multiplyNumber(8,3)
+divNumber(12,4)
